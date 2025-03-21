ac6c3f65d1d3b7319da78dd4c4f23b95e5a0ba89
Candela Echazu <gcandela894@gmail.com>
2025-03-21 16:57:42 -0300

    corregimos firebasemodule

--- src/firebase/firebase.module.ts
+++ src/firebase/firebase.module.ts
@@ -1,41 +1,8 @@
 import { Module } from '@nestjs/common';
-import * as admin from 'firebase-admin';
 import { FirebaseService } from './firebase.service';
 
-
 @Module({
-  providers: [
-    FirebaseService,
-    {
-      provide: 'FIRESTORE',
-      useFactory: async () => {
-        const requiredEnvVars = [
-          'FIREBASE_PROJECT_ID',
-          'FIREBASE_PRIVATE_KEY',
-          'FIREBASE_CLIENT_EMAIL',
-        ];
-
-        for (const envVar of requiredEnvVars) {
-          if (!process.env[envVar]) {
-            throw new Error(`La variable de entorno ${envVar} no está definida.`);
-          }
-        }
-
-        const serviceAccount: admin.ServiceAccount = {
-          projectId: process.env.FIREBASE_PROJECT_ID!,
-          privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
-          clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
-        };
-
-        // Inicializar Firebase Admin SDK
-        const app = admin.initializeApp({
-          credential: admin.credential.cert(serviceAccount),
-        });
-
-        return app.firestore();
-      },
-    },
-  ],
-  exports: ['FIRESTORE'],
+  providers: [FirebaseService],
+  exports: [FirebaseService],
 })
 export class FirebaseModule {}
